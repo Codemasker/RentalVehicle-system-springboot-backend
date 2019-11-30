e7a0fa49e006155c6918c5d04eeba29ed7b7a126
Code masker <codemasker@gmail.com>
2019-11-30 22:29:59 +0530

    merge : JUnit

--- src/test/java/lk/haniman/rentalsystem/RestApiTest.java
+++ src/test/java/lk/haniman/rentalsystem/RestApiTest.java
@@ -1,12 +1,37 @@
 package lk.haniman.rentalsystem;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class RestApiTest {
 
-    private static MockMvc mvc;
+    private MockMvc getMvc;
 
-    public static void main(String[] args) {
+    @InjectMocks
+    private Main main;
 
+    @Before
+    public void setUp() throws Exception{
+        getMvc = MockMvcBuilders.standaloneSetup(main).build();
+
+    }
+
+    @Test
+    public void testVehicleList() throws Exception{
+        getMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hehejeje"));
     }
 }
